e7a94154358f39199e4976de85c432f14ff7cc3a
Oluwafunmilayo Adewodu <funmiadewodu@yahoo.com>
2017-08-03 12:19:54 +0100

    Adding jquery

--- interface.js
+++ interface.js
@@ -1,23 +1,37 @@
 $(document).ready(function(){
   var thermostat = new Thermostat();
-  $("#temperature").text(thermostat.temperature());
 
-  $("#temperature-up").click(function(){
-      thermostat.up();
-      $("#temperature").text(thermostat.temperature());
+  $('#temperature').text(thermostat.temperature());
+
+  $('#temperature-up').on('click', function() {
+    thermostat.up();
+    $('#temperature').text(thermostat.temperature());
+    updateTemperature();
   });
 
-  $("#temperature-down").click(function(){
-      thermostat.down();
-      $("#temperature").text(thermostat.temperature());
-  });
-  $("#temperature-reset").click(function(){
-      thermostat.resetTemperature();
-      $("#temperature").text(thermostat.temperature());
+  $('#temperature-down').on('click',function() {
+    thermostat.down();
+    $('#temperature').text(thermostat.temperature());
+    updateTemperature();
   });
 
+  $('#temperature-reset').on('click', function(){
+    thermostat.resetTemperature();
+    $('#temperature').text(thermostat.temperature());
+  });
 
+  $('#powersaving-on').on('click', function(){
+    thermostat.powerSavingModeOn()
+    $('#power-saving-status').text('on');
+  });
 
+  $('#powersaving-off').on('click', function(){
+    thermostat.powerSavingModeOff()
+    $('#power-saving-status').text('off');
+  });
 
+  function updateTemperature() {
+    $('#temperature').attr('class', thermostat.getEnergyUsage());
+  }
 
 });
